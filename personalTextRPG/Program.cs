cb54397b3f44f71313fbc633b0f3d84d4d66b9b4
yndoo <isabelle2k@naver.com>
2025-02-03 09:20:43 +0900

    Initial commit

---
+++ personalTextRPG/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace personalTextRPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
